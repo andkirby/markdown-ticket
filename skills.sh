0b760a8caeadce13dbef6488db0c1254d78e5240
Kirby Rs <bizkirby@gmail.com>
2026-03-01 16:51:06 +0100

    chore: added skills registry document + skills loader files

---
+++ skills.sh
@@ -0,0 +1,3 @@
+npx skills add -y https://github.com/vercel-labs/agent-skills --skill vercel-react-native-skills
+npx skills add https://github.com/anthropics/skills --skill frontend-design -y
+npx skills add microsoft/playwright-cli@playwright-cli -i
